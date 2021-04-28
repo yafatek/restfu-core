3dd643b26c874b7176c6d7ccdce6cfdfcca73c8e
Feras <>
2021-04-28 23:56:14 +0300

    init

---
+++ src/test/java/dev/yafatek/restcore/RestCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yafatek.restcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
