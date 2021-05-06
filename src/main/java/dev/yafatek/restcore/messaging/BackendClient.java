694176d399611830c309127119ea15f4598b700c
Feras <>
2021-05-07 00:32:43 +0300

    comments

--- src/main/java/dev/yafatek/restcore/messaging/BackendClient.java
+++ src/main/java/dev/yafatek/restcore/messaging/BackendClient.java
@@ -16,21 +16,24 @@ public interface BackendClient {
      * Method to Call remote Method on targeted Service based on the DirectExchange and following the RoutingKey to bind the
      * response to the publisher source.
      *
-     * @param payload    Pojo Object of Type T
-     * @param routingKey specific key to bind the request to a specific queue
-     * @param <T>        type of pojo Object usually the request body
-     * @param <R>        the return type, its usually represents the response pojo Object
+     * @param payload      Pojo Object of Type T
+     * @param routingKey   specific key to bind the request to a specific queue
+     * @param exchangeName the target exhcnage name
+     * @param <T>          type of pojo Object usually the request body
+     * @param <R>          the return type, its usually represents the response pojo Object
      * @return pojo object represents the service response
      */
     @SendTo
     <T, R> R sendWithAsync(T payload, String routingKey, String exchangeName);
 
+
     /**
-     * Method To Call Get Services Without Parameters.
+     * Method to publish events with ASYNC To RabbitMQ.
      *
-     * @param routingKey destination Method at remote Server
-     * @param <R>        Return Type, usually the response that the remote service return.
-     * @return remote service response type.
+     * @param routingKey   target key
+     * @param exchangeName exchange name
+     * @param <R>          return type
+     * @return POJO as JSON
      */
     <R> R sendWithAsync(String routingKey, String exchangeName);
 
