045a31d88889739685bc8734a0e4bde83a0e1a82
feraso0o0 <ceo@yafatek.dev>
2021-05-25 01:16:57 +0300

    add notifications support

---
+++ src/main/java/dev/yafatek/restcore/notifications/DestinationType.java
@@ -0,0 +1,19 @@
+package dev.yafatek.restcore.notifications;
+
+/**
+ * notification type specifies a target platform, web admin or mobile.
+ *
+ * @author Feras E Alawadi
+ * @version 1.0.101
+ * @since 1.0.101
+ */
+public enum DestinationType {
+    /**
+     * notifications for mobile applications.
+     */
+    MOBILE,
+    /**
+     * notifications For Web Administration panel
+     */
+    WEB_ADMIN
+}
---
+++ src/main/java/dev/yafatek/restcore/notifications/NotificationCodes.java
@@ -0,0 +1,19 @@
+package dev.yafatek.restcore.notifications;
+
+/**
+ * Notifications RabbitMQ Codes
+ *
+ * @author Feras E. Alawadi
+ * @version 10.0.101
+ * @since 1.0.101
+ */
+public class NotificationCodes {
+    /**
+     * Notifications Default Exchange.
+     */
+    public static final String NOTIFICATIONS_EXCHANGE = "notifications:exchange";
+    /**
+     * Key to Pull Notifications
+     */
+    public static final String NOTIFICATIONS_PULL = "notifications.pullNotification";
+}
